abfd14c9075b5a008a9f59b858451e7f5cd55782
Administrator <klevis@resulaj.eu>
2022-04-02 19:11:59 +0200

    Added in repo

--- Projects/Lists/buy_computer_improved.py
+++ Projects/Lists/buy_computer_improved.py
@@ -9,7 +9,7 @@ current_choice = "_"
 computer_parts = []  # create an empty list
 
 while current_choice != '0':
-    if current_choice in "123456":
+    if current_choice in "1234567":
         print("Adding {}".format(current_choice))
         if current_choice == '1':
             computer_parts.append("computer")
@@ -23,11 +23,12 @@ while current_choice != '0':
             computer_parts.append("mouse-pad")
         elif current_choice == '6':
             computer_parts.append("hdmi-cable")
+        elif current_choice == '7':
+            computer_parts.append("dvd-drive")
     else:
         print("Please add options from the list below:")
-        for part in available_parts:
-
-            print("{0}: {1}".format(available_parts.index(part) + 1, part))
+        for number, part in enumerate(available_parts):
+            print("{0}: {1}".format(number + 1, part))
     current_choice = input()
 print(computer_parts)
 print(' '.join(computer_parts))

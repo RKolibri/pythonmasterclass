e771f853dc3a2e61863c7bfa19243fbd82b914c4
Administrator <klevis@resulaj.eu>
2022-04-02 18:39:22 +0200

    Added in repo

--- Projects/Lists/lists.py
+++ Projects/Lists/lists.py
@@ -1,13 +1,18 @@
 pages = ["Home", "About", "Features", "Contact"]
+
 for i in pages:
     print(i)
+
 print()
 print(pages[-1], (pages[3]))
 print(' '.join(pages))
+
 pages += ["FAQ"]
 print(pages)
+
 pages_new = pages
 print(' '.join(pages_new))
+
 a = b = c = d = e = f = g = pages = pages_new
 print("Navigation Bar")
 f.append("Login")

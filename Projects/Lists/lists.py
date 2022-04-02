ceecdbc3b31c1d8b83c8a19ec32b1dbeba0bc882
Administrator <klevis@resulaj.eu>
2022-04-02 18:37:06 +0200

    Added in repo

--- Projects/Lists/lists.py
+++ Projects/Lists/lists.py
@@ -1,6 +1,19 @@
 pages = ["Home", "About", "Features", "Contact"]
-for page in pages:
-    print(page)
+for i in pages:
+    print(i)
 print()
-print(pages[2], (pages[3]))
-print(pages[-2], (pages[-3]))
+print(pages[-1], (pages[3]))
+print(' '.join(pages))
+pages += ["FAQ"]
+print(pages)
+pages_new = pages
+print(' '.join(pages_new))
+a = b = c = d = e = f = g = pages = pages_new
+print("Navigation Bar")
+f.append("Login")
+print(g)
+print(b)
+f.append("Register")
+
+print("Final Navigation Bar")
+print(' '.join(d))
---
+++ Projects/Lists/number_lists.py
@@ -0,0 +1,14 @@
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+print(min(even))
+print(max(even))
+print(min(odd))
+print(max(odd))
+
+print()
+print(len(even), "even numbers")
+print(len(odd), "odd numbers")
+
+print()
+print("Mississippi".count("issi"))
